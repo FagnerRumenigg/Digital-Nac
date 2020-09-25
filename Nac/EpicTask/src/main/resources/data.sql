a6a238b4d4e029c06bf052dd8689d0d75410d35c
FagnerRumenigg <rumeniggmoraes@gmail.com>
2020-09-25 14:59:37 -0300

    nac

---
+++ Nac/EpicTask/src/main/resources/data.sql
@@ -0,0 +1,26 @@
+--USUÁRIOS
+INSERT INTO users (name, git_hub_user, email, pass) VALUES
+('Michael Myers', 'Michael-Myers', 'michaelmyers@killers.com', 'NickCastle');
+
+INSERT INTO users (name, git_hub_user, email, pass) VALUES
+('Jason Voorheers', 'Jason-Voorheers', 'jasonvoorheers@@killers.com', 'KaneHooder');
+
+INSERT INTO users (name, git_hub_user, email, pass) VALUES
+('GhostFace', 'Ghost-Face', 'ghostface@killers.com', 'RobertEnglund');
+
+INSERT INTO users (name, git_hub_user, email, pass) VALUES
+('Freddy Krueger', 'Freddy-Krueger', 'freddykrueger@killers.com', 'RogerL.Jackson');
+
+INSERT INTO users (name, git_hub_user, email, pass) VALUES
+('Pennywise', 'Dancer-Clown', 'pennywise@killers.com', 'Bill Skarsgård');
+
+
+-- TAREFAS
+INSERT INTO tasks(title, description, point, status) VALUES
+('Matar', 'Assasinar as pessoas', 60, 100);
+
+INSERT INTO tasks(title, description, point, status) VALUES
+('Assustar', 'Deixar as pessoas com medo', 80, 0);
+
+INSERT INTO tasks(title, description, point, status) VALUES
+('Perseguir', 'Correr atrás das pessoas', 100, 36);
